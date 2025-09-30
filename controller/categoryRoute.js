89be9fb5ed319948d84974b414e8a3eb498e6546
KienHa2805 <s4112914@rmit.edu.vn>
2025-09-30 11:10:54 +0700

    Things updated: 1. Adding category creating route and category deleting route 2. Adding favicon image

--- controller/categoryRoute.js
+++ controller/categoryRoute.js
@@ -5,11 +5,40 @@ const Category = require('../model/category-schema');
 //GET all categories
 router.get('/', async (req,res) => {
     try{
-        const categories = await Category.find();
+        const categories = await Category.find({});
         res.render('category', {
             categories: categories
         });
     } catch (err) {
+        res.status(500).send('Server Error');
+        console.error(err);
+    }
+});
+
+//POST new category
+router.post('/', async (req,res)=> {
+    try{
+        let { categoryName, categoryDescription, categoryImage } = req.body;
+        let category = new Category({
+            name : categoryName,
+            description : categoryDescription,
+            image : categoryImage
+        });
+        await category.save();
+        res.redirect('/category');
+    } catch (err) {
+        res.status(500).send('Server Error');
+        console.error(err);
+    }
+});
+
+//POST delete category
+router.post('/delete/:id', async(req,res)=>{
+    try{
+        const categoryId = req.params.id;
+        await Category.findByIdAndDelete(categoryId);
+        res.redirect('/category');
+    }catch (err) {
         res.status(500).send('Server Error');
         console.error(err);
     }
