d876e2a76d0bc2d8304a39fe00ee404c0a7c84e7
KienHa2805 <s4112914@rmit.edu.vn>
2025-10-07 10:42:32 +0700

    Adding formatted price inputs in Vietnamese

--- controller/productRoute.js
+++ controller/productRoute.js
@@ -1,38 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../model/product-schema');
+const Category = require('../model/category-schema');
 
 //GET products of a specific category
 router.get('/:categoryId', async (req,res) => {
     try {
         const categoryId = req.params.categoryId;
         const products = await Product.find({ category: categoryId });
+        const categoryName = await Category.findById(categoryId).select('name');
         res.render('product-detail', {
             products: products,
-            categoryId: categoryId
+            categoryId: categoryId,
+            categoryName: categoryName.name
         });
     }catch (err) {
         res.status(500).send('Server Error');
         console.error(err);
     }
 });
+
 //POST add a new product
 router.post('/:categoryId',async(req,res) => {
-    try {
-        const {productName, productWeight, productLength, productWidth, productThick, priceIn, priceOut, pricePer } = req.body;
+    try{
         const categoryId = req.params.categoryId;
-        const newProduct = new Product ({
+        
+        // Clean formatted price inputs (remove dots)
+        const cleanPriceIn = req.body.priceIn ? parseInt(req.body.priceIn.replace(/\./g, '')) : null;
+        const cleanPriceOut = req.body.priceOut ? parseInt(req.body.priceOut.replace(/\./g, '')) : null;
+        const cleanPricePer = req.body.pricePer ? parseInt(req.body.pricePer.replace(/\./g, '')) : null;
+        
+        let { productName, productWeight, productLength, productWidth, productThick } = req.body;
+        let product = new Product({
             name: productName,
-            priceIn: priceIn,
-            priceOut: priceOut,
-            pricePer: pricePer,
             weight: productWeight,
+            length: productLength,
             width: productWidth,
             thickness: productThick,
-            length: productLength,
+            priceIn: cleanPriceIn,
+            priceOut: cleanPriceOut,
+            pricePer: cleanPricePer,
             category: categoryId
         });
-        await newProduct.save();
+        await product.save();
         res.redirect('/product/' + categoryId);
     } catch (err) {
         res.status(500).send('Server Error');
@@ -54,22 +64,28 @@ router.post('/delete/:categoryId/:productId', async(req,res) => {
 });
 
 //POST update a product
-router.post ('/update/:categoryId/:productId', async function (req,res) {
+router.post('/update/:categoryId/:productId', async function (req,res) {
     try{
-        const {productName, productWeight, productLength, productWidth, productThick, priceIn, priceOut, pricePer } = req.body;
         const productId = req.params.productId;
         const categoryId = req.params.categoryId;
+        
+        // Clean formatted price inputs
+        const cleanPriceIn = req.body.priceIn ? parseInt(req.body.priceIn.replace(/\./g, '')) : null;
+        const cleanPriceOut = req.body.priceOut ? parseInt(req.body.priceOut.replace(/\./g, '')) : null;
+        const cleanPricePer = req.body.pricePer ? parseInt(req.body.pricePer.replace(/\./g, '')) : null;
+        
+        let { productName, productWeight, productLength, productWidth, productThick } = req.body;
         await Product.findByIdAndUpdate(productId, {
             name: productName,
-            priceIn: priceIn,
-            priceOut: priceOut,
-            pricePer: pricePer,
             weight: productWeight,
+            length: productLength,
             width: productWidth,
             thickness: productThick,
-            length: productLength
+            priceIn: cleanPriceIn,
+            priceOut: cleanPriceOut,
+            pricePer: cleanPricePer
         });
-        res.redirect(`/product/${categoryId}`);
+        res.redirect('/product/' + categoryId);
     } catch (err) {
         res.status(500).send('Server Error');
         console.error(err);
